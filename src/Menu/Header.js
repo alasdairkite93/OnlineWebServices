f4c9e010fa967c68de468ff7a19bdc0a4d969a89
alasdairkite93 <alasdairkite93@gmail.com>
2024-07-08 20:23:03 +0100

    Uploaded Logo

--- src/Menu/Header.js
+++ src/Menu/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <header>
       <div className="max-w-full flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="logo">
-            <h1>Online Web Services</h1>
+            <img style={{width: "300px"}} src="https://res.cloudinary.com/dqehq7lmd/image/upload/v1720466488/cleanlogo_hmtkrb.png"/>
         </Link>
         <Navbar />
       </div>
